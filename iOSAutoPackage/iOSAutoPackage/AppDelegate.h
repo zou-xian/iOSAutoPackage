92d8171ed5a2c810c9b55b7da7fbf97552d47046
zouxian <zouxian183@163.com>
2020-05-12 11:53:58 +0800

    init

---
+++ iOSAutoPackage/iOSAutoPackage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSAutoPackage
+//
+//  Created by zouxian on 2020/5/12.
+//  Copyright © 2020 zouxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOSAutoPackage/iOSAutoPackage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSAutoPackage
+//
+//  Created by zouxian on 2020/5/12.
+//  Copyright © 2020 zouxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
